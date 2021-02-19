a98289cb52b963237aabbf5e7e7a9c4bf287e766
MPR <235953@edu.p.lodz.pl>
2021-02-19 12:09:07 +0100

    02 Zmiana dodawania na mnożenie w funkcji "Policz"

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int Policz(int a, int b);
+
 int main() {
-    printf("Hello, World!\n");
+    printf("2 * 3 = %d", Policz(2, 3));
     return 0;
+}
+
+int Policz(int a, int b){
+    return a*b;
 }
